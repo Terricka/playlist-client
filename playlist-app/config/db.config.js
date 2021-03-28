bdf3057c47e10593f108889f0d81e4b33c529cc2
Terricka <kumbaya@Terrickas-MacBook-Pro-2.local>
2021-03-28 12:55:52 -0400

    Fetching from Database

---
+++ playlist-app/config/db.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "SA",
+    PASSWORD: "IllM@tic08",
+    DB: "PlaylistApp",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
